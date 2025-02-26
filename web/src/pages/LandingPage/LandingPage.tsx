20ec3fefeaac68355e68242619bf18f98ff3b4b0
klickers <bluebirid228@gmail.com>
2025-02-26 21:55:23 +0200

    add landing page

---
+++ web/src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,20 @@
+import {Metadata} from "@redwoodjs/web"
+
+const LandingPage = () => {
+    return (
+        <>
+            <Metadata
+                title="Home"
+                description="Welcome to your personal hub."
+            />
+            <div className="text-center pt-6 pb-12 min-h-screen flex flex-col justify-center items-center">
+                <h1>Welcome to your personal hub.</h1>
+                <p className="max-w-lg">
+                    An all-in-one creativity and productivity workspace.
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default LandingPage
