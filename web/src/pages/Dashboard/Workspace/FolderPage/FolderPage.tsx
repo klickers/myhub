92f068b63699f3a1042f70bf959d8315595b1fef
klickers <bluebirid228@gmail.com>
2025-03-22 20:43:57 +0200

    add TasksCell to folder pages

--- web/src/pages/Dashboard/Workspace/FolderPage/FolderPage.tsx
+++ web/src/pages/Dashboard/Workspace/FolderPage/FolderPage.tsx
@@ -1,11 +1,14 @@
 import FolderCell from "src/components/Workspace/FolderCell"
 import ProjectsCell from "src/components/Workspace/ProjectsCell"
+import TasksCell from "src/components/Workspace/TasksCell"
 
 const FolderPage = ({ slug }) => {
     return (
         <>
             <FolderCell slug={slug} />
             <ProjectsCell parentSlug={slug} />
+            <div className="mt-10"></div>
+            <TasksCell parentSlug={slug} />
         </>
     )
 }
