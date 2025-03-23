f26ed72766dbcedd1148933700a6f7bedbe19be4
klickers <bluebirid228@gmail.com>
2025-03-23 12:50:24 +0200

    move calendar route

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -11,13 +11,13 @@ const Routes = () => {
                 <Set wrap={DashboardLayout} title="Dashboard">
                     <Route path="/dashboard" page={DashboardIndexPage} name="dashboard" />
                     <Route path="/dashboard/time-tracking" page={DashboardTimeTrackingIndexPage} name="timeTracking" />
+                    <Route path="/dashboard/calendar" page={DashboardCalendarIndexPage} name="calendar" />
                     <Set wrap={WorkspaceLayout} title="Workspace">
                         <Route path="/dashboard/workspace" page={DashboardWorkspaceIndexPage} name="workspace" />
                         <Route path="/dashboard/workspace/folder/{slug}" page={DashboardWorkspaceFolderPage} name="workspaceFolder" />
                         <Route path="/dashboard/workspace/project/{slug}" page={DashboardWorkspaceProjectPage} name="workspaceProject" />
                         <Route path="/dashboard/workspace/project/{slug}/update" page={DashboardWorkspaceUpdateProjectPage} name="workspaceUpdateProject" />
                     </Set>
-                    <Route path="/dashboard/calendar" page={DashboardCalendarIndexPage} name="calendar" />
                 </Set>
             </PrivateSet>
             <Route path="/" page={LandingPage} name="landing" />
