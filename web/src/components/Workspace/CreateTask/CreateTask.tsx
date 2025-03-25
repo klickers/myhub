7b17c5285daef6fdcee7c6f345cc2576ecdb9950
klickers <bluebirid228@gmail.com>
2025-03-25 22:34:29 +0200

    Update CreateTask.tsx

--- web/src/components/Workspace/CreateTask/CreateTask.tsx
+++ web/src/components/Workspace/CreateTask/CreateTask.tsx
@@ -86,6 +86,7 @@ const CreateTask = ({ parentId, tasks, setTasks }: Props) => {
                 ...data,
                 softDueDate,
                 dueDate,
+                sessions: [],
             } as Item,
         ])
         modalRef.current.closeModal()
