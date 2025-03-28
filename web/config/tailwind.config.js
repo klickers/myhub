cc71727d63d2cd258d5d70671080d3536a83e700
klickers <bluebirid228@gmail.com>
2025-03-28 18:10:53 +0300

    add safelist

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["src/**/*.{js,jsx,ts,tsx}"],
+    safelist: [
+        "border-none",
+        {
+            pattern: /bg-+/,
+        },
+    ],
     theme: {
         extend: {
             fontFamily: {
